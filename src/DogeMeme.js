549a7e7e8f6d197869b6655ddf7ff779107d3f31
Ricardo M. Alvarado <ricardoma97@gmail.com>
2019-03-10 14:58:56 -0600

    added inputs for dynamic text

--- src/DogeMeme.js
+++ src/DogeMeme.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react';
 import './DogeMeme.css';
 
 class DogeMeme extends Component {
+	state = {
+		top: '',
+		bottom: ''
+	}
+
+	onChangeText = (e) => {
+		let prop = e.target.name;
+		let value = e.target.value;
+		this.setState({
+			[prop]: value
+		});
+	}
+
 	render() {
 		return (
 			<div className='doge-container'>
+				<div className='doge-form'>
+					<input
+						className="doge-input" 
+						name="top"
+						type="text"
+						value={this.state.top}
+						onChange={this.onChangeText}
+						placeholder="Here goes top text"
+					/>
+					<input
+						className="doge-input" 
+						name="bottom"
+						type="text"
+						value={this.state.bottom}
+						onChange={this.onChangeText}
+						placeholder="Here goes bottom text"
+					/>
+				</div>
+
 				<div className='doge-meme doge-container'>
 					<div className="doge-text">
-						I'm a Harcoded
+						{this.state.top}
 					</div>
 					<div className="doge-text">
-						Text!!
+						{this.state.bottom}
 					</div>
 				</div>
 			</div>
