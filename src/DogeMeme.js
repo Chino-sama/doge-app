40392638077b004c51276b09bc009a6d2b8a91f5
Ricardo M. Alvarado <ricardoma97@gmail.com>
2019-03-10 14:35:37 -0600

    added doge and hardcoded text

---
+++ src/DogeMeme.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './DogeMeme.css';
+
+class DogeMeme extends Component {
+	render() {
+		return (
+			<div className='doge-container'>
+				<div className='doge-meme doge-container'>
+					<div className="doge-text">
+						I'm a Harcoded
+					</div>
+					<div className="doge-text">
+						Text!!
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default DogeMeme;
